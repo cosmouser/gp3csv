2c8ec49903acc7d654184ac8d53a02048c357923
Cosmo Martinez <cosmo@ucsc.edu>
2019-06-23 11:00:15 -0700

    tests start and directorySize values

--- hpi/hpi.go
+++ hpi/hpi.go
@@ -3,11 +3,12 @@ package hpi
 import (
 	"io"
 	"fmt"
+	"encoding/binary"
 )
 
 type header struct {
 	marker [4]byte // HAPI
-	saveMaker [4]byte // BANK if a save
+	saveMarker [4]byte // BANK if a save
 	directorySize uint32 // The size of the directory
 	headerKey uint32 // Decryption key
 	start uint32 // File offset of directory
@@ -19,6 +20,24 @@ func loadHeader(r io.Reader) (h header, err error) {
 			return h, err
 		}
 		return h, fmt.Errorf("expecting to read %v bytes, only read %v bytes", len(h.marker), n)
+	}
+	if n, err := r.Read(h.saveMarker[:]); err != nil || n != len(h.saveMarker) {
+		if err != nil {
+			return h, err
+		}
+		return h, fmt.Errorf("expecting to read %v bytes, only read %v bytes", len(h.saveMarker), n)
+	}
+	err = binary.Read(r, binary.LittleEndian, &h.directorySize)
+	if err != nil {
+		return
+	}
+	err = binary.Read(r, binary.LittleEndian, &h.headerKey)
+	if err != nil {
+		return
+	}
+	err = binary.Read(r, binary.LittleEndian, &h.start)
+	if err != nil {
+		return
 	}
 	return
 }
