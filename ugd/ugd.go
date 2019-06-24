d1d3ca05b5db15b76785ca0183f9a3d1156a8bc3
Cosmo Martinez <cosmo@ucsc.edu>
2019-06-24 11:20:22 -0700

    tdd run 1 addBuildRelationships

--- ugd/ugd.go
+++ ugd/ugd.go
@@ -1,8 +1,38 @@
 package ugd
 
+import (
+	"github.com/cosmouser/tdf"
+	"path"
+	"strings"
+	"bytes"
+)
+
 const (
 	escUnitsDir = "unitsE"
 	escWeaponsDir = "weaponE"
 	escUnitpicsDir = "unitpicE"
-	escDownloadsDir = "downloadsE
+	escDownloadsDir = "downloadsE"
 )
+
+func loadTdfDataDir(store map[string][]byte, dir string) (nodes []*tdf.Node, err error) {
+	fileNames := []string{}
+	dirPath := path.Join("/", dir)
+	for k := range store {
+		if strings.Index(k, dirPath) == 0 {
+			fileNames = append(fileNames, k)
+		}
+	}
+	for _, fileName := range fileNames {
+		if ext := strings.ToLower(path.Ext(fileName)); ext == ".tdf" || ext == ".ota" || ext == ".fbi" {
+			tmp, err := tdf.Decode(bytes.NewReader(store[fileName]))
+			if err != nil {
+				return nodes, err
+			}
+			for _, v := range tmp {
+				nodes = append(nodes, v)
+			}
+		}
+	}
+	return
+}
+
--- ugd/ugd_test.go
+++ ugd/ugd_test.go
@@ -18,5 +18,22 @@ func TestPrintUnit(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+	if len(db["/unitsE/ARMCOM.fbi"]) == 0 {
+		t.Error("missing file")
+	}
+	nodes, err := loadTdfDataDir(db, escUnitsDir)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(nodes) == 0 {
+		t.Error("got 0 for unit count, expected at least a couple hundred")
+	}
+	for _, v := range nodes {
+		if v.Fields["unitname"] == "CORAK" {
+			if v.Fields["copyright"] != "Copyright 1997 Humongous Entertainment. All rights reserved." {
+				t.Errorf("%v is missing copyright, has %v", v.Fields["unitname"], v.Fields["copyright"])
+			}
+		}
+	}
 	f.Close()
 }
